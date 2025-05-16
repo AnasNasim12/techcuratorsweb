850f27db647f6388a69c9e683811e2ec4ea1807f
Tarungupta2606 <tarun.gupta2606@gmail.com>
2025-05-16 14:38:33 +0530

    more new

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: false, // disable dark mode completely
+  darkMode: false, // <--- force disable dark mode
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
