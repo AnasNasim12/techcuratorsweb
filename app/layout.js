a5a02155b06b1169c62fd9331fb33901b0842f86
Tarungupta2606 <tarun.gupta2606@gmail.com>
2025-05-30 16:06:11 +0530

    layout.js fix

--- app/layout.js
+++ app/layout.js
@@ -28,6 +28,15 @@ const geistMono = Geist_Mono({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script id="clarity" strategy="beforeInteractive">
+          {`(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "n7q03mty84");`}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
@@ -40,14 +49,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','GTM-KR79DX3H');`}
         </Script>
 
-        {/* Microsoft Clarity */}
-        <Script id="clarity" strategy="beforeInteractive">
-          {`(function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "n7q03mty84");`}
-        </Script>
+       
 
         {/* Google Tag Manager noscript */}
         <noscript>
